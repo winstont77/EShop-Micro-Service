c4f65bc86017a0774031efd12eeb9e97e2d73f4a
winstont77 <45140188+winstont77@users.noreply.github.com>
2024-05-21 12:01:47 +0800

    mediatR + Carter base

---
+++ src/Services/Catalog/Catalog.API/Models/Products/CreateProduct/CreateProductEndPoint.cs
@@ -0,0 +1,18 @@
+﻿using Carter;
+
+namespace Catalog.API.Models.Products.CreateProduct;
+
+public record CreateProductRequest(string Name,
+                                   List<string> Category,
+                                   string Description,
+                                   string ImageFile,
+                                   decimal Price);
+
+public record CreateProductResponse(Guid Id);
+public class CreateProductEndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Models/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,36 @@
+﻿using BuildingBlocks.CQRS;
+using MediatR;
+
+namespace Catalog.API.Models.Products.CreateProduct;
+
+public record CreateProductCommand(string Name,
+                                   List<string> Category,
+                                   string Description,
+                                   string ImageFile,
+                                   decimal Price) : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        //create Product entity from command object
+        //save to database
+        //Return CreateProductResult result
+
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        //TODO
+        //save to database
+        //return result
+
+        return new CreateProductResult(Guid.NewGuid());
+    }
+}
