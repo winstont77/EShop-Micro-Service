ce99e458d7dbdea1ff9644ec8188fb977b405e76
winstont77 <45140188+winstont77@users.noreply.github.com>
2024-06-09 18:12:15 +0800

    basket

---
+++ src/Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,32 @@
+using BuildingBlocks.Behaviors;
+using BuildingBlocks.Exceptions.Handler;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipline
+app.MapCarter();
+app.UseExceptionHandler(option => { });
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
